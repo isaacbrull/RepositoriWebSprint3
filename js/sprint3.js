5dec8270f10ae0d5dfc4cdda03eeff33eb8e3751
isaacbrull <61147625+isaacbrull@users.noreply.github.com>
2021-01-15 17:32:26 +0100

    Arrays acavat per consola

--- js/sprint3.js
+++ js/sprint3.js
@@ -1,13 +1,22 @@
-let frutas = ["Manzana", "Banana"]
-
-console.log(frutas.length)
-
-var alumne = new Array(3)
+var alumne = new Array(6)
 
 alumne[0] = "Ximo"
 alumne[1] = "Isaac"
 alumne[2] = "Sofian"
+alumne[3] = "Joel"
+alumne[4] = "Hideo Kojima"
+alumne[5] = "Juanito"
 
 for (i=0;i<alumne.length;i++){
-   	console.log("L'alumne " + alumne[i] + " és situa a el " + i + " de la llista.")
+   	console.log("L'alumne " + alumne[i] + " és situa a el " + i + " de la llista d'alumnes registrats.")
 }
+var professor = new Array(3)
+
+professor[0] = "Alex"
+professor[1] = "Joan"
+professor[2] = "Toni"
+
+for (i=0;i<professor.length;i++){
+   	console.log("El professor " + professor[i] + " és situa a el " + i + " de la llista de professors registrats.")
+}
+

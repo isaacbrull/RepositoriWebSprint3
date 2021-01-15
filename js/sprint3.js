1ea5d0cb8c1aeb37c1a03e1117503792daba6910
joaquimbernaltes <51153324+joaquimbernaltes@users.noreply.github.com>
2021-01-15 17:25:50 +0100

    Javascript Arrays
    arrays

--- js/sprint3.js
+++ js/sprint3.js
@@ -1,3 +1,13 @@
 let frutas = ["Manzana", "Banana"]
 
 console.log(frutas.length)
+
+var alumne = new Array(3)
+
+alumne[0] = "Ximo"
+alumne[1] = "Isaac"
+alumne[2] = "Sofian"
+
+for (i=0;i<alumne.length;i++){
+   	console.log("L'alumne " + alumne[i] + " és situa a el " + i + " de la llista.")
+}

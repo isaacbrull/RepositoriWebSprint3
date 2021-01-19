294364dc14f587979b02d86c7800db61b11a27b3
isaacbrull <61147625+isaacbrull@users.noreply.github.com>
2021-01-19 19:00:06 +0100

    prova de sidebar

--- perfil.php
+++ perfil.php
@@ -43,11 +43,19 @@
 	<div class="banner text-center text-white"><h3 class="d-inline bg-dark px-2 py-1">PROVA</h3></div>
 
   <?php if($currentuser['Tipus'] !==  'Professor' || 'Alumne' || null): ?>
-  <p><a href="./perfil.php?tab=0">Gestió de professors</a></p>
+    <div class="w3-sidebar w3-light-grey w3-bar-block" style="width:25%">
+  <h3 class="w3-bar-item">Menu</h3>
+  <a href="./perfil.php?tab=0" class="w3-bar-item w3-button">Gestió de professors</a>
+  <a href="./perfil.php?tab=1" class="w3-bar-item w3-button">Gestió de alumnes</a>
+  <a href="./perfil.php?tab=2" class="w3-bar-item w3-button">Gestió de grups</a>
+  
+</div>
+  <!--<p><a href="./perfil.php?tab=0">Gestió de professors</a></p>
   <p><a href="./perfil.php?tab=1">Gestió de alumnes</a></p>
-  <p><a href="./perfil.php?tab=2">Gestió de grups</a></p>
+  <p><a href="./perfil.php?tab=2">Gestió de grups</a></p>-->
         <?php endif; ?>
-  <p><a href="./perfil.php?tab=3">Gestió de propostes</a></p>
+        <a href="./perfil.php?tab=3" class="w3-bar-item w3-button">Gestió de propostes</a>
+  <!--<p><a href="./perfil.php?tab=3">Gestió de propostes</a></p>-->
       </section>
     </div>
     <div class="body-column-right">
